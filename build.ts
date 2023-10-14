ee37217c807edaf3be5232885b67826357584127
Kin <120244424+k1iin@users.noreply.github.com>
2023-10-14 11:20:18 +0700

    setup build

--- build.ts
+++ build.ts
@@ -3,9 +3,22 @@ import { parse } from "https://deno.land/std@0.204.0/flags/mod.ts";
 const args = parse(Deno.args);
 
 /** Key yang diawali dengan `$` akan diganti dengan tipe environment*/
-export function buildEnv(keys: string[], export?: boolean): Record<string, string> {
+export function buildEnv(keys: string[], exportValue?: boolean): Record<string, string> {
+  const environment = (Deno.env.get("GH_REF") as string == Deno.env.get("GH_DEFAULT_BRANCH")) ? "PROD" : "PREVIEW";
   const env = {};
-  for (const key of keys) {
-    //TODO(k1iin) setup tipe environment
+  
+  for (const rawKey of keys) {
+    const key = rawKey.startsWith("$") ? rawKey.replace("$", `${environment}_`) : rawKey;
+    const cleanedKey = rawKey.replace("$", "");
+    
+    if (Deno.env.has(key)) {
+      const value = Deno.env.get(key) as string;
+      env[cleanedKey] = value
+      if (exportValue) exportVariable(cleanedKey, value);
+    } else {
+      console.warn(`Tidak ada env dengan key "${key}"`)
+    }
   }
+
+  return env;
 };
