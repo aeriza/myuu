66bfcb33203e4657b88e5de6bced6e2f354cda63
Kin <120244424+k1iin@users.noreply.github.com>
2023-10-14 10:21:47 +0700

    Create main file

---
+++ main.ts
@@ -0,0 +1,28 @@
+import tweetnacl from "npm:tweetnacl@1.0.3";
+import { InteractionResponseType } from "discord_api_types";
+
+async function handler(request: Request): Promise<Response> {
+  const invalidResponse = new Response("Invalid Request", { status: 401 });
+  const headers = ["X-Signature-Ed25519", "X-Signature-Timestamp"];
+  if (request.method != "POST" && !headers.every(request.headers.get)) return invalidResponse;
+    
+  const [signature, timestamp]: [string, string] = headers.every(request.headers.get);
+  const body = await request.text();
+  const valid = tweetnacl.sign.detached.verify(
+    new TextEncoder().encode(timestamp + body),
+    hexToUint8Array(signature),
+    hexToUint8Array(Deno.env.get("PUBLIC_KEY") as string)
+  );
+
+  if (!valid) return invalidResponse;
+
+  return new Response(
+    JSON.stringify({ type: InteractionResponseType.Pong })
+  );
+}
+
+function hexToUint8Array(hex: string): Uint8Array {
+  return new Uint8Array(
+    hex.match(/.{1,2}/g)!.map((ctx) => parseInt(ctx, 16))
+  );
+}
