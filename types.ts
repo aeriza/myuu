6eac8c22c5519fdeb0e03e44603e12d62aebecd4
Aurezia Intan C <140772578+aeriza@users.noreply.github.com>
2023-10-15 19:36:15 +0700

    update typings

--- types.ts
+++ types.ts
@@ -1,19 +1,23 @@
 import {
   type APIChatInputApplicationCommandInteraction,
+  type APIInteraction,
   type APIMessageApplicationCommandInteraction,
   type APIUserApplicationCommandInteraction,
   ApplicationCommandType,
+  type InteractionType,
   type RESTPostAPIChatInputApplicationCommandsJSONBody,
   type RESTPostAPIContextMenuApplicationCommandsJSONBody
 } from "discord_api_types";
 import type { REST } from "@discordjs/rest";
+
+export type RunResult = Promise<any> | Response;
 
 export type Command =
   | { data: RESTPostAPIChatInputApplicationCommandsJSONBody, execute(interaction: APIChatInputApplicationCommandInteraction, options: Partial<RunOptions>): RunResult }
   | { data: Partial<RESTPostAPIContextMenuApplicationCommandsJSONBody> & { type: ApplicationCommandType.Message }, execute(interaction: APIMessageApplicationCommandInteraction, options: Partial<RunOptions>): RunResult }
   | { data: Partial<RESTPostAPIContextMenuApplicationCommandsJSONBody> & { type: ApplicationCommandType.User }, execute(interaction: APIUserApplicationCommandInteraction, options: Partial<RunOptions>): RunResult }
 
-export type RunResult = Promise<Response> | Response;
+export type Interaction = { type: InteractionType, execute(interaction: APIInteraction): RunResult };
 
 export interface RunOptions {
   rest: REST;
